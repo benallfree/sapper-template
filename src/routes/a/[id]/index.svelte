f8415af5ca04158efcefba269db2cce935c4761d
Thomas Ghysels <info@thomasg.be>
2019-03-12 15:10:34 +0100

    Repro 606

---
+++ src/routes/a/[id]/index.svelte
@@ -0,0 +1,8 @@
+<a href="a/same">a/same</a>
+<a href="b/same">b/same</a>
+<br>
+page: a/{$page.params.id}
+
+<script>
+  import { page } from '@sapper/app'
+</script>
---
+++ src/routes/b/[id]/index.svelte
@@ -0,0 +1,8 @@
+<a href="a/same">a/same</a>
+<a href="b/same">b/same</a>
+<br>
+page: b/{$page.params.id}
+
+<script>
+  import { page } from '@sapper/app'
+</script>
